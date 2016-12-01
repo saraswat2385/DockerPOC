a11ce5d4481715717945596cdb2682c9c772f36b
Sandeep Rawat <sandeep@opstree.com>
2016-12-01 23:24:15 +0530

    Updated the test repo reference

--- sandy/jobdsl/RepoCICreator.groovy
+++ sandy/jobdsl/RepoCICreator.groovy
@@ -4,7 +4,7 @@ job('CI Generator') {
   parameters {
     stringParam('PROJ_ID', 'ContinuousIntegration', 'Project identifier')
     stringParam('REPO_BRANCH', 'master', 'Branch on which CI needs to be done')
-    stringParam('GIT_URL', 'https://github.com/OpsTree/ContinuousIntegration.git', 'URL of git repo for which CI setup need to be done')
+    stringParam('GIT_URL', 'https://github.com/XbaPAASP/Spring3HibernateApp.git', 'URL of git repo for which CI setup need to be done')
   }
   steps {
     shell('#!/bin/bash \n source ${JENKINS_HOME}/data/scripts/file_function.sh \n echo "Doing the CI setup of repo ${GIT_URL}" \n echo "Doing the CI setup of repo ${GIT_URL}" \n create_jobdsl_file_from_template ${PROJ_ID} ${REPO_BRANCH} ${GIT_URL} CodeCoverage')
