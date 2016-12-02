7c3427a4af0f01e1de5573efede757a77bff26d7
Sandeep Rawat <sandeep@opstree.com>
2016-12-02 20:23:15 +0530

    Jobdsl to generate plugin for project status on dashboard portlet:

---
+++ sandy/jobdsl/ProjectStatsPluginGenerator.groovy
@@ -0,0 +1,20 @@
+mavenJob('ProjectStatsPluginGenerator') {
+  description("I'll generate ProjectStatsPlugin")
+  logRotator(-1, 10)
+  scm {
+    git {
+    branch('*/master')
+      remote {
+        name('')
+        refspec('')
+        url('https://github.com/XbaPAASP/project-stats-plugin.git')
+      }
+    }
+  }
+  //please enable the maven in your jenkins form jenkins configuration
+  goals('clean package')
+  rootPOM('pom.xml')
+  postBuildSteps {
+    shell("cp -v \${WORKSPACE}/target/project-stats-plugin.hpi ${JENKINS_HOME}/plugins/")
+  }
+}
