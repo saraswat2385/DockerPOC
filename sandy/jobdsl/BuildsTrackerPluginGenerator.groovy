eeaae063bc0ac5312d2ebc4897640ff7c360d804
Sandeep Rawat <sandeep@opstree.com>
2016-12-04 12:10:23 +0530

    Created another job for build tracking

---
+++ sandy/jobdsl/BuildsTrackerPluginGenerator.groovy
@@ -0,0 +1,20 @@
+mavenJob('BuildsTrackerPluginGenerator') {
+  description("I'll generate BuildsTrackerPlugin")
+  logRotator(-1, 10)
+  scm {
+    git {
+    branch('*/master')
+      remote {
+        name('')
+        refspec('')
+        url('https://github.com/XbaPAASP/buildstracker.git')
+      }
+    }
+  }
+  //please enable the maven in your jenkins form jenkins configuration
+  goals('clean package')
+  rootPOM('pom.xml')
+  postBuildSteps {
+    shell("cp -v \${WORKSPACE}/target/buildstracker.hpi ${JENKINS_HOME}/plugins/")
+  }
+}
